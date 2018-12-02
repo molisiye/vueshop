79bb8c5eba640610f62f66a470e4c577f1d76cf4
molisiye <molisiye@live.com>
2018-12-02 19:25:18 +0800

    配置代理实现跨域 ajax 请求

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,8 +7,13 @@
 
 <script>
 import FooterGuide from './components/FooterGuide/FooterGuide.vue';
+import {reqFoodTypes} from './api'
 
 export default {
+  async mounted () {
+    const result = await reqFoodTypes()
+    console.log(result)
+  },
   components: {
     FooterGuide
   }
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,13 +5,14 @@
 
 import ajax from './ajax'
 
+const base_url = '/api'
 // 1、根据经纬度获取位置详情
-export const reqAddress = (geohash) => ajax('/position/${geohash}')
+export const reqAddress = (geohash) => ajax('${base_url}/position/${geohash}')
 
 // 2、 获取食品分类列表
-export const reqFoodTypes = () => ajax('/index_category/')
+export const reqFoodTypes = () => ajax(base_url + '/index_category/')
 // 3、根据经纬度获取商铺列表
-export const reqShops = (longitude, latitude) => ajax('/shops', {longitude, latitude})
+export const reqShops = (longitude, latitude) => ajax('${base_url}/shops', {longitude, latitude})
 
 // 4、根据经纬度和关键字搜索商铺列表
 
